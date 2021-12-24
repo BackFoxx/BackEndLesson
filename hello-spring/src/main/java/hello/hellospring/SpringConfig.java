a76e6e4b2e350d49cf7a7588d68771cd8af64c18
BackFoxx <jorealbusiness@gmail.com>
2021-12-25 06:38:38 +0900

    JPA

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,22 +1,26 @@
 package hello.hellospring;
 
+import hello.hellospring.Repository.JDBC_TemplateMemberRepository;
 import hello.hellospring.Repository.JDBCmemberRepository;
+import hello.hellospring.Repository.JPA_MemberRepository;
 import hello.hellospring.Repository.MemberRepository;
 import hello.hellospring.Service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
-    private DataSource dataSource;
+    private final DataSource dataSource;
+    private EntityManager em;
 
-    @Autowired
-    public SpringConfig(DataSource dataSource) {
+    public SpringConfig(DataSource dataSource, EntityManager em) {
         this.dataSource = dataSource;
+        this.em = em;
     }
 
     @Bean
@@ -26,7 +30,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new JDBCmemberRepository(dataSource);
+        return new JPA_MemberRepository(em);
     }
 
 }
