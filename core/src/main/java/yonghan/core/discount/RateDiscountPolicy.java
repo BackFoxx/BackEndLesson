4057ab5996b262bf71fc102484e8acd51d533c5a
BackFoxx <jorealbusiness@gmail.com>
2022-01-05 11:19:20 +0900

    @ComponentScan, @Component, @Autowired로 스프링 컨테이너 등록, 의존관계 자동 주입하기

--- core/src/main/java/yonghan/core/discount/RateDiscountPolicy.java
+++ core/src/main/java/yonghan/core/discount/RateDiscountPolicy.java
@@ -1,8 +1,10 @@
 package yonghan.core.discount;
 
+import org.springframework.stereotype.Component;
 import yonghan.core.member.Grade;
 import yonghan.core.member.Member;
 
+@Component
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private int discountPercent = 10;
