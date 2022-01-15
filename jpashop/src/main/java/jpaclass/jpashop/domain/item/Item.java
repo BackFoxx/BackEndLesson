29f7deed0d9ac6d743d9f5f2072c62cacdac06fc
BackFoxx <jorealbusiness@gmail.com>
2022-01-15 22:57:06 +0900

    상품 도메인 개발

--- jpashop/src/main/java/jpaclass/jpashop/domain/item/Item.java
+++ jpashop/src/main/java/jpaclass/jpashop/domain/item/Item.java
@@ -1,8 +1,8 @@
 package jpaclass.jpashop.domain.item;
 
 import jpaclass.jpashop.domain.Category;
+import jpaclass.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
-import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -11,7 +11,7 @@ import java.util.List;
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype")
-@Getter @Setter
+@Getter
 public abstract class Item {
 
     @Id
@@ -25,4 +25,16 @@ public abstract class Item {
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
 
+    //비즈니스 로직
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    } //stock 증가
+
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more Stock");
+        }
+        this.stockQuantity = restStock;
+    } //stock 감소
 }
