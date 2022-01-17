0a4fbaa881860f9fa16ba2cd81d1d450e8fab4f8
BackFoxx <jorealbusiness@gmail.com>
2022-01-18 04:10:17 +0900

    상품 목록, 상품 수정

--- jpashop/src/test/java/jpaclass/jpashop/Main.java
+++ jpashop/src/test/java/jpaclass/jpashop/Main.java
@@ -1,22 +1,12 @@
 package jpaclass.jpashop;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+        int M = s.nextInt();
         int N = s.nextInt();
 
-
-    }
-
-    public static int Logic(int N) {
-        List<Integer> lists = new ArrayList<>();
-        int i = 2;
-        while (N != 1) {
-            if ()
-        }
     }
 }
