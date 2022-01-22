f985538ffa316861a1d5f75da6302cb8b49d26be
BackFoxx <jorealbusiness@gmail.com>
2022-01-22 13:20:42 +0900

    lifecycle

--- exam31/src/exam/TenServlet.java
+++ exam31/src/exam/TenServlet.java
@@ -6,16 +6,16 @@ import javax.servlet.annotation.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "Servlet", value = "/Servlet")
+@WebServlet("/ten")
 public class TenServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("test/html;charset=UTF-8");
+        response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
         out.print("<h1>1-10까지 출력!!</h1>");
-//        for (int i = 0; i < 10; i++) {
-//            out.println(i+"<br>");
-//        }
-//        out.close();
+        for (int i = 1; i <= 10; i++) {
+            out.println(i+"<br>");
+        }
+        out.close();
     }
 }
