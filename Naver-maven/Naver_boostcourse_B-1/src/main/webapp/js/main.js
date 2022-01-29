22a723113e0f3e99314a8e13478e79f1a3e21d01
BackFoxx <jorealbusiness@gmail.com>
2022-01-29 23:04:55 +0900

    DOM 문제로 고생중

--- Naver-maven/Naver_boostcourse_B-1/src/main/webapp/js/main.js
+++ Naver-maven/Naver_boostcourse_B-1/src/main/webapp/js/main.js
@@ -5,10 +5,11 @@ function todoType(e) {
     const http = new XMLHttpRequest();
     const todoId = e.target.getAttribute("todo_id");
 
-    http.onreadystatechange = (e) => {
+    http.onreadystatechange = () => {
         if (http.readyState === http.DONE) {
             if (http.status === 200 || http.status === 201) {
                 console.log("SUCCESS!");
+                todoMainUpdate(e.target);
             } else {
                 console.log("Fail");
             }
@@ -19,5 +20,17 @@ function todoType(e) {
     http.send();
 }
 
-function todoMainUpdate(e) {
+function todoMainUpdate(btn) {
+    const parent = btn.parentElement.parentElement;
+    const className = parent.getAttribute('id');
+
+    console.log(className);
+    const updatedSection = '<div class="Section_todo_box">\n' +
+        '                    <span class="todo_title">${todolists.title}</span>\n' +
+        '                    <span class="todo_desc">${todolists.regDate}, ${todolists.name}, ${todolists.sequence}</span>\n' +
+        '                    <button todo_id="${todolists.id}" class="todo_btn">→</button>\n' +
+        '                </div>'
+    if (className === 'Section_todo') {
+        parent.insertAdjacentHTML("beforeend", updatedSection);
+    }
 }
