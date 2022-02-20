04650488f40c619be8d978c1b67288f4ca554b0e
BackFoxx <jorealbusiness@gmail.com>
2022-02-21 08:20:23 +0900

    reviews 완성

--- Naver_boostcourse_C-1/src/main/webapp/static/js/detail_shortReview.js
+++ Naver_boostcourse_C-1/src/main/webapp/static/js/detail_shortReview.js
@@ -2,7 +2,10 @@ function GetAjaxToShortReviewJs() {
     const response = AJAX.displayInfoAjax;
     PutAverageScore(response.averageScore);
     PutShortPreview(response.comments);
-    setMoreBtnPath();
+
+    SHORT_REVIEW.moreBtn.addEventListener('click', (e) => {
+        sendDisplayInfoToReview(e, response);
+    });
 }
 
 const SHORT_REVIEW = {
@@ -22,53 +25,57 @@ function PutAverageScore(averageScore) {
     SHORT_REVIEW.averageScoreNumber.innerHTML = averageScore.toFixed(1);
 }
 
-function PutShortPreview(commentlist) {
-    SHORT_REVIEW.reviewCount.innerHTML = `${commentlist.length}건`;
+function PutShortPreview(commentList) {
+    SHORT_REVIEW.reviewCount.innerHTML = `${commentList.length}건`
 
-    const shortCommentlist = [];
-    for (var i = 0; i < SHORT_REVIEW.shortReviewCount; i++) {
-        shortCommentlist.push(commentlist[i]);
+    if (commentList.length > 0) {
+        const shortCommentlist = [];
+        for (var i = 0; i < SHORT_REVIEW.shortReviewCount; i++) {
+            shortCommentlist.push(commentList[i]);
+        }
+
+        shortCommentlist.forEach((comment) => {
+            const obj = {
+                comment: comment.comment,
+                score: parseFloat(comment.score).toFixed(1),
+                reservationDate: function () {
+                    const dateForm = comment.reservationDate.split('-');
+                    const year = dateForm[0];
+                    const month = dateForm[1];
+                    const day = dateForm[2].split(' ')[0]
+                    return `${year}.${month}.${day}`;
+                },
+                email: function () {
+                    const email = comment.reservationEmail;
+                    if (email.length > 4) {
+                        return email.substr(0, 4) + '****';
+                    }
+                },
+                image: function () {
+                    if (comment.commentImage !== null) {
+                        return comment.commentImage[0].saveFileName;
+                    } else {
+                        return false;
+                    }
+                },
+                imageCount: function () {
+                    if (comment.commentImage !== null) {
+                        return comment.commentImage.length;
+                    } else {
+                        return false;
+                    }
+                },
+
+            };
+
+            const bindTemplate = Handlebars.compile(SHORT_REVIEW.shortReviewTemplate);
+            SHORT_REVIEW.shortReviewList.innerHTML += bindTemplate(obj);
+        });
     }
-
-    shortCommentlist.forEach((comment) => {
-        const obj = {
-            comment: comment.comment,
-            score: parseFloat(comment.score).toFixed(1),
-            reservationDate: function () {
-                const dateForm = comment.reservationDate.split('-');
-                const year = dateForm[0];
-                const month = dateForm[1];
-                const day = dateForm[2].split(' ')[0]
-                return `${year}.${month}.${day}`;
-            },
-            email: function () {
-                const email = comment.reservationEmail;
-                if (email.length > 4) {
-                    return email.substr(0, 4) + '****';
-                }
-            },
-            image: function () {
-                if (comment.commentImage !== null) {
-                    return comment.commentImage[0].saveFileName;
-                } else {
-                    return false;
-                }
-            },
-            imageCount: function () {
-                if (comment.commentImage !== null) {
-                    return comment.commentImage.length;
-                } else {
-                    return false;
-                }
-            },
-
-        };
-
-        const bindTemplate = Handlebars.compile(SHORT_REVIEW.shortReviewTemplate);
-        SHORT_REVIEW.shortReviewList.innerHTML += bindTemplate(obj);
-    });
 }
 
-function setMoreBtnPath() {
-    SHORT_REVIEW.moreBtn.setAttribute('href', `review.html?id=${AJAX.displayInfoId}`);
+function sendDisplayInfoToReview(e, displayInfo) {
+    e.preventDefault()
+    sessionStorage.setItem('displayInfo', JSON.stringify(displayInfo));
+    window.location.href = 'review.html';
 }
--- Naver_boostcourse_C-1/src/main/webapp/static/js/review.js
+++ Naver_boostcourse_C-1/src/main/webapp/static/js/review.js
@@ -1,7 +1,8 @@
-function GetAjaxToShortReviewJs() {
-    const response = AJAX.displayInfoAjax;
-    log(response);
-}
+document.addEventListener('DOMContentLoaded', () => {
+    const displayInfo = JSON.parse(sessionStorage.getItem('displayInfo'))
+    PutAverageScore(displayInfo.averageScore);
+    PutReview(displayInfo.comments, displayInfo.displayInfo.productDescription);
+});
 
 const REVIEW = {
     averageScoreStar: document.querySelector('.graph_value'),
@@ -12,6 +13,53 @@ const REVIEW = {
     ReviewList: document.querySelector('.list_short_review'),
 }
 
-function log(response) {
-    console.log(response);
+function PutAverageScore(averageScore) {
+    REVIEW.averageScoreStar.style.width = `${(averageScore * 20).toFixed(1)}%`;
+    REVIEW.averageScoreNumber.innerHTML = averageScore.toFixed(1);
 }
+
+function PutReview(commentList, productDescription) {
+    REVIEW.reviewCount.innerHTML = `${commentList.length}건`
+
+    if (commentList.length > 0) {
+        commentList.forEach((comment) => {
+            const obj = {
+                Description: productDescription,
+                comment: comment.comment,
+                score: parseFloat(comment.score).toFixed(1),
+                reservationDate: function () {
+                    const dateForm = comment.reservationDate.split('-');
+                    const year = dateForm[0];
+                    const month = dateForm[1];
+                    const day = dateForm[2].split(' ')[0]
+                    return `${year}.${month}.${day}`;
+                },
+                email: function () {
+                    const email = comment.reservationEmail;
+                    if (email.length > 4) {
+                        return email.substr(0, 4) + '****';
+                    }
+                },
+                image: function () {
+                    if (comment.commentImage !== null) {
+                        return comment.commentImage[0].saveFileName;
+                    } else {
+                        return false;
+                    }
+                },
+                imageCount: function () {
+                    if (comment.commentImage !== null) {
+                        return comment.commentImage.length;
+                    } else {
+                        return false;
+                    }
+                },
+
+            };
+
+            const bindTemplate = Handlebars.compile(REVIEW.ReviewTemplate);
+            REVIEW.ReviewList.innerHTML += bindTemplate(obj);
+        });
+    }
+}
+
