c2fdc06c27a881acddb978e5d3492fae649e2c09
조승현 <jorealbusiness@gmail.com>
2022-03-02 08:39:06 +0900

    youtube security - sprinf auth

---
+++ youtude/security1/src/main/java/com/example/security1/controller/IndexController.java
@@ -0,0 +1,73 @@
+package com.example.security1.controller;
+
+import com.example.security1.model.User;
+import com.example.security1.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequiredArgsConstructor
+public class IndexController {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder encoder;
+
+    @GetMapping({"", "/"})
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    public String manager() {
+        return "manager";
+    }
+
+    @GetMapping("/loginForm")
+    public String loginForm() {
+        return "loginForm";
+    }
+
+    @GetMapping("/joinForm")
+    public String joinForm() {
+        return "/joinForm";
+    }
+
+    @PostMapping("/join")
+    public String join(User user) {
+        user.setRole("ROLE_USER");
+        user.setPassword(encoder.encode(user.getPassword()));
+        userRepository.save(user);
+        return "redirect:/loginForm";
+    }
+
+    @Secured("ROLE_ADMIN")
+    @GetMapping("/info")
+    public @ResponseBody
+    String info() {
+        return "개인정보";
+    }
+
+    @PreAuthorize("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+    @GetMapping("/data")
+    public @ResponseBody
+    String data() {
+        return "데이터 정보";
+    }
+
+}
