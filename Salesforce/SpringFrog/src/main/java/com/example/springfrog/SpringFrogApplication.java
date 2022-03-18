58f36b75a58daa84e7ddc9b84383bc0221943662
조승현 <jorealbusiness@gmail.com>
2022-03-18 22:26:46 +0900

    또 다른 프로젝트

---
+++ Salesforce/SpringFrog/src/main/java/com/example/springfrog/SpringFrogApplication.java
@@ -0,0 +1,13 @@
+package com.example.springfrog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFrogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFrogApplication.class, args);
+    }
+
+}
