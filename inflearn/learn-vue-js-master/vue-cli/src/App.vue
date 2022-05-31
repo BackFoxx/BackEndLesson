d41fb2f7ca448a5dbbe31cead6117f4b3019788c
조승현 <jorealbusiness@gmail.com>
2022-05-31 20:08:32 +0900

    컴퓨터 교체 전 임시저장

---
+++ inflearn/learn-vue-js-master/vue-cli/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <app-header 
+      v-bind:str="str"
+      v-on:renew="renewStr">
+    </app-header>
+    {{str}}
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue'
+
+export default {
+  data: function () {
+    return {
+      str: 'header!!'  
+    }
+  },
+  components: {
+    'app-header': AppHeader
+  },
+  methods: {
+    renewStr: function() {
+      this.str = 'hi';
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ inflearn/learn-vue-js-master/vue-form/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <form v-on:submit.prevent="submitForm">
+      <div>
+        <label for="username">id: </label>
+        <input id="username" type="text" v-model="username">
+      </div>
+      <div>
+        <label for="password">password: </label>
+        <input id="password" type="text" v-model="password">
+      </div>
+      <button type="submit">login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data: function() {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    submitForm: function () {
+      // event.preventDefault();
+      console.log(this.username);
+      console.log(this.password);
+      const url = 'https://jsonplaceholder.typicode.com/users';
+      const data = {
+        username: this.username,
+        password: this.password
+      }
+      axios.post(url, data)
+        .then(function(resoponse) {
+          console.log(resoponse);
+        })
+        .catch(function(error) {
+          console.log(error);
+        })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
